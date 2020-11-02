400d35fb6ff1a538feac3b5bdda63dccce2d1ada
unknown <CarlosColindres>
2020-11-02 18:33:09 -0500

    reducers

---
+++ src/components/TodoForm.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+
+const TodoForm = ({addTodo, clearTodos}) => {
+    
+    const [newTodo, setNewTodo] = useState('')
+
+    const onChange = (e) => {
+        setNewTodo(e.target.value)
+    }
+
+    const formSubmit = (e) => {
+        e.preventDefault()
+         addTodo(newTodo)
+        setNewTodo('')
+    }
+
+        return (
+            <div>
+                <form onSubmit={formSubmit}>
+                    <input 
+                    type="text" 
+                    name="todo"
+                    placeholder='Add todo'
+                    value= {newTodo}
+                    onChange={onChange}
+                    />
+                    <button>Add Todo</button>
+                    <button onClick={clearTodos}>Clear Completed</button>
+                </form>
+            </div>
+        )
+}
+
+export default TodoForm
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Todo from './Todo'
+
+const TodoList = ({todoList, finishTodo}) => {
+    
+    return (
+        <div>
+            {todoList.map(todo => <Todo key={todo.id} todo={todo} finishTodo={finishTodo}/>)}
+        </div>
+    )
+}
+
+export default TodoList
